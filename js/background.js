2ccb415762651c003c60d83d6ce8ad573527877a
papadog218 <mki218@naver.com>
2022-01-17 00:13:47 +0900

    add CSS

--- js/background.js
+++ js/background.js
@@ -1,9 +1,17 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images =[
+    "img01.jpg",
+    "img02.jpg",
+    "img03.jpg",
+    "img04.jpg",
+    "img05.jpg"
+];
+const BG = "background"
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
+bgImage.className = BG;
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `img/${chosenImage} `;
 
 document.body.appendChild(bgImage);
--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,23 @@
-const weather = document.querySelector("#weather span:first-child");
-const city = document.querySelector("#weather span:last-child");
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const API_KEY = "a0be9911b60dfebce9c1ed3f9a6d458f";
 
-function onGeoOk(position) {
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp} ℃ `;
     });
 }
-function onGeoError() {
-  alert("Can't find you. No weather for you.");
+
+function onGeoError(){
+    alert("Can't find you. No weather for you");
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
